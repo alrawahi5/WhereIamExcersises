d98c404e0900221901c9ecd544fa199eaa012f50
ABDULLAH <abdullah555523@hotmail.com>
2024-02-28 13:13:52 +0400

    added a new java file and initialized a scanner

---
+++ WhereIam/ValidatePalindrom.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ValidatePalindrom {
+
+    public static void main(String [] args){
+        String word;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the tested word: ");
+        word = scanner.nextLine();
+        System.out.println(word);
+
+        
+    }
+}
