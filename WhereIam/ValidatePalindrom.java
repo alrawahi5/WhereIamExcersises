d1db67ca8f77c0a10a3d19c48e7a08d5d50e475f
ABDULLAH <abdullah555523@hotmail.com>
2024-02-29 12:27:23 +0400

    adding the Palindrom task

--- WhereIam/ValidatePalindrom.java
+++ WhereIam/ValidatePalindrom.java
@@ -1,14 +1,42 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class ValidatePalindrom {
-
-    public static void main(String [] args){
-        String word;
+    public static boolean validPalindrome() {
+        boolean flag = true;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the tested word: ");
-        word = scanner.nextLine();
-        System.out.println(word);
+        String word = scanner.nextLine().toLowerCase().replaceAll("[^\\\\sa-zA-Z0-9]", "");
+//        System.out.println(word);
+        Stack<Character> myCharWord = new Stack<Character>();
+        Stack<Character> myCharWordRev = new Stack<Character>();
 
-        
+        for (int i = 0; i<word.length(); i++) {
+            myCharWord.push(word.charAt(i));
+            }
+        //System.out.println("Original: " + myCharWord);
+
+        for (int i = 0; i<word.length()+1; i++) {
+            if (!myCharWord.empty()) {
+                myCharWordRev.push(myCharWord.pop());
+            }
+        }
+        //System.out.println("Rev: "+myCharWordRev);
+
+        for (int c = 0; c<myCharWordRev.size(); c++) {
+            if (myCharWord.push(myCharWordRev.pop()).equals(myCharWordRev.elementAt(c))) {
+                System.out.println("yes");
+                return flag;
+            } else {
+                System.out.println("no");
+                flag = false;
+                return flag;
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String [] args){
+        validPalindrome();
     }
 }
