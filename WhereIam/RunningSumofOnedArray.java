68239b23a125fcabf0d46317c6303eca83b49178
ABDULLAH <abdullah555523@hotmail.com>
2024-03-05 12:17:00 +0400

    added the task for running the sam of an arry

---
+++ WhereIam/RunningSumofOnedArray.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RunningSumofOnedArray {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int arrySize = 0;
+
+        System.out.println("Specify the size of the array: ");
+        if (scanner.hasNext()){
+            arrySize = scanner.nextInt();
+        }
+        int[] nubers = new int[arrySize];
+
+        int count = 0;
+        System.out.println("Fill in your Array of size " + arrySize + ": " + "\nelement #: " + count++);
+        //scanner.nextInt();
+        for (int i = 0; i < arrySize; i++){
+            if(scanner.hasNext()){
+                System.out.println("elemet #: " + count++);
+                nubers[i] = scanner.nextInt();
+            }
+        }
+        System.out.println("Your Array contains the following elements:");
+        for (int i = 0; i < arrySize; i++){
+            System.out.print(nubers[i]  + " ");
+        }
+        System.out.println();
+        System.out.println("the summed array is: ");
+        int[] sumArray = new int[arrySize];
+        int increment = 0;
+
+        for (int i = 0; i<arrySize; i++){
+            sumArray[i] = nubers[i]+increment;
+            increment = increment+nubers[i];
+            System.out.print(sumArray[i]  + " ");
+        }
+    }
+}
